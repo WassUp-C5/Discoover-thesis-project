a4c52f7c2e9bf5ed2272bd5011e9f45f193e31a0
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-10-15 22:05:06 +0100

    serving angular app with express

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname + "./../dist/DiscooverIn/"));
+
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
 })
