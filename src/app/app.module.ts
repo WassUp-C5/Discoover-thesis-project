8f69ce713f7ec46abfbe63723e8e5f58216ad742
Slim Kasraoui <slim.kasraoui2020@gmail.com>
2020-10-15 21:53:39 +0100

    visitor & guide navbars OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,59 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { LandingPageComponent } from './landing-page/landing-page.component';
+// import {MatButtonModule} from '@angular/material/button';
+// import { SearchTripComponent } from './search-trip/search-trip.component';
+// import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
+// import { GuideATripComponent } from './guide-a-trip/guide-a-trip.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LandingPageComponent,
+//     SearchTripComponent,
+//     OrganizeATripComponent,
+//     GuideATripComponent
+//   ],
+//   imports: [
+
+//     BrowserModule,
+//     AppRoutingModule,
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+import { VisitorNavbarComponent } from './visitor-navbar/visitor-navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { SearchTripComponent } from './search-trip/search-trip.component';
-import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
-import { GuideATripComponent } from './guide-a-trip/guide-a-trip.component';
+import { GuideNavBarComponent } from './guide-navbar/guide-navbar.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
-    SearchTripComponent,
-    OrganizeATripComponent,
-    GuideATripComponent
+    VisitorNavbarComponent,
+    GuideNavBarComponent,
   ],
   imports: [
-   
     BrowserModule,
     AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/visitor-navbar/visitor-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'visitor-navbar',
+  templateUrl: './visitor-navbar.component.html',
+  styleUrls: ['./visitor-navbar.component.css']
+})
+export class VisitorNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
