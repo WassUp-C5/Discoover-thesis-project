0ef44098b62b8a1d02881bfec1c049551f941a0b
Med-Amine-Louati <medamine505@live.fr>
2020-10-16 10:30:28 +0100

    fixing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GuideATripComponent } from './guide-a-trip/guide-a-trip.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
+import { SearchTripComponent } from './search-trip/search-trip.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '', component:LandingPageComponent},
+  {path :'searchTrip', component:SearchTripComponent},
+  {path :'OrganizeTrip',component:OrganizeATripComponent},
+  {path :'GuideTrip', component :GuideATripComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -9,12 +8,6 @@ import { SearchTripComponent } from './search-trip/search-trip.component';
 import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
 import { GuideATripComponent } from './guide-a-trip/guide-a-trip.component';
 
-const routes : Routes = [
-  {path: '', component:LandingPageComponent},
-  {path :'searchTrip', component:SearchTripComponent},
-  {path :'OrganizeTrip',component:OrganizeATripComponent},
-  {path :'GuideTrip', component :GuideATripComponent}
-]
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +17,7 @@ const routes : Routes = [
     GuideATripComponent
   ],
   imports: [
-    RouterModule.forRoot(routes),
+   
     BrowserModule,
     AppRoutingModule,
   ],
