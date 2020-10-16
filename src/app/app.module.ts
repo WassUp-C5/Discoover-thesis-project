f9cd393155018667c45e05527414d422511bf2f0
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-10-16 14:00:04 +0100

    generating signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { SearchTripComponent } from './search-trip/search-trip.component';
 import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
 import { GuideATripComponent } from './guide-a-trip/guide-a-trip.component';
 import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -53,7 +54,8 @@ import { SigninComponent } from './signin/signin.component';
     SearchTripComponent,
     OrganizeATripComponent,
     GuideATripComponent,
-    SigninComponent
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
 
