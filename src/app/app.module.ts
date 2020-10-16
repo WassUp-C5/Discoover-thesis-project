57444374acf53c5fa36706eed0b9e0084807a2ed
Med-Amine-Louati <medamine505@live.fr>
2020-10-16 09:36:18 +0100

    adding three component with routing working correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import {MatButtonModule} from '@angular/material/button';
+import { SearchTripComponent } from './search-trip/search-trip.component';
+import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
+import { GuideATripComponent } from './guide-a-trip/guide-a-trip.component';
 
 const routes : Routes = [
-  {path: '', component:LandingPageComponent}
+  {path: '', component:LandingPageComponent},
+  {path :'searchTrip', component:SearchTripComponent},
+  {path :'OrganizeTrip',component:OrganizeATripComponent},
+  {path :'GuideTrip', component :GuideATripComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SearchTripComponent,
+    OrganizeATripComponent,
+    GuideATripComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
