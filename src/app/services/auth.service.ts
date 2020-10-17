6dea6abe35c78ee48ac7b8682c0c54b1c042bb3a
Saber-Bjeoui <bjeoui.saber@gmail.com>
2020-10-17 11:32:21 +0100

    adding new User class model

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { User } from './../models/User';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  authUrl = "/api/auth";
   constructor(private http: HttpClient) {}
 
-  signup() {
-    this.http.post('/api/auth/signup', this.data);
+  signup(user:User):Observable<User> {
+    return this.http.post<User>(this.authUrl + '/signup', user, httpOptions);
   }
 }
