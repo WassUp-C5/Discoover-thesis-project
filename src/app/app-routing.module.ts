9348cc1b79d82293a210b6e4caa893a2f1955787
Med-Amine-Louati <medamine505@live.fr>
2020-10-19 16:44:22 +0100

    add trip done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,20 +7,19 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
 import { SearchTripComponent } from './search-trip/search-trip.component';
 import { SigninComponent } from './signin/signin.component';
-import {SignupComponent} from './signup/signup.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path: '', component:LandingPageComponent},
-  {path :'searchTrip', component:SearchTripComponent},
-  {path :'organizeTrip',component:OrganizeATripComponent},
-  {path :'guideTrip', component :GuideATripComponent},
-  {path : 'signup',component:SignupComponent},
-  {path : 'signin',component:SigninComponent}
-
+  { path: '', component: LandingPageComponent },
+  { path: 'searchTrip', component: SearchTripComponent },
+  { path: 'organizeTrip', component: OrganizeATripComponent },
+  { path: 'guideTrip', component: GuideATripComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
