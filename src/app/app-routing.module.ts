5a11cba82ef72437e31ef1f314b9f09eadc4e22b
Slim Kasraoui <slim.kasraoui2020@gmail.com>
2020-10-20 10:50:07 +0100

    restructuring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GuideNavBarComponent } from './guide-navbar/guide-navbar.component';
-import { OrganizerProfileComponent } from './organizer-profile/organizer-profile.component';
-import { GuideATripComponent } from './guide-a-trip/guide-a-trip.component';
+import { GuideNavBarComponent } from './guide/guide-navbar/guide-navbar.component';
+import { OrganizerProfileComponent } from './organizer/organizer-profile/organizer-profile.component';
+import { GuideATripComponent } from './guide/guide-a-trip/guide-a-trip.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { OrganizeATripComponent } from './organize-a-trip/organize-a-trip.component';
+import { OrganizeATripComponent } from './organizer/organize-a-trip/organize-a-trip.component';
 import { SearchTripComponent } from './search-trip/search-trip.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
