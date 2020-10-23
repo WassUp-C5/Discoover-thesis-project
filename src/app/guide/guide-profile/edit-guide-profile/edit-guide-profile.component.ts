33ebff1b10f6e1f7653772227a7202c9852c4c1f
Saber <bjeoui.saber@gmail.com>
2020-10-23 14:35:55 +0100

    test

--- src/app/guide/guide-profile/edit-guide-profile/edit-guide-profile.component.ts
+++ src/app/guide/guide-profile/edit-guide-profile/edit-guide-profile.component.ts
@@ -1,9 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+<<<<<<< HEAD
+=======
+import { TokenStorageService } from './../../../services/token-storage.service';
+import { ActivatedRoute } from '@angular/router';
+>>>>>>> test
 
 @Component({
   selector: 'app-edit-guide-profile',
   templateUrl: './edit-guide-profile.component.html',
+<<<<<<< HEAD
   styleUrls: ['./edit-guide-profile.component.css']
 })
 export class EditGuideProfileComponent implements OnInit {
@@ -17,6 +23,40 @@ export class EditGuideProfileComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+=======
+  styleUrls: ['./edit-guide-profile.component.css'],
+})
+export class EditGuideProfileComponent implements OnInit {
+  @Input() guide;
+
+  guidep;
+  currentUser:any;
+  isLoggedIn:boolean;
+  guideId:string;
+  language:string = '';
+  selectedLevel:string = '';
+
+  constructor(
+    private http: HttpClient,
+    private tokenStorage: TokenStorageService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    if(this.tokenStorage.getUser()){
+      this.currentUser = this.tokenStorage.getUser();
+      this.isLoggedIn = true;
+    }
+    else {
+      this.isLoggedIn = false;
+    }
+
+    this.route.params.subscribe(params => {
+      this.guideId = params['id']; // (+) converts string 'id' to a number
+
+      // In a real app: dispatch action to load the details here.
+   });
+>>>>>>> test
   }
 
   saveData() {
