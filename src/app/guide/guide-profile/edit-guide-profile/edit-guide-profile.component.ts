c3db93694d4fbf5f7ee48a027e9472117f6c777a
Saber <bjeoui.saber@gmail.com>
2020-10-23 16:54:41 +0100

    fix guide profile

--- src/app/guide/guide-profile/edit-guide-profile/edit-guide-profile.component.ts
+++ src/app/guide/guide-profile/edit-guide-profile/edit-guide-profile.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 import { ActivatedRoute } from '@angular/router';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-guide-profile',
@@ -12,11 +13,13 @@ export class EditGuideProfileComponent implements OnInit {
   @Input() guide;
 
   guidep;
-  currentUser:any;
-  isLoggedIn:boolean;
-  guideId:string;
-  language:string = '';
-  selectedLevel:string = '';
+  currentUser: any;
+  isLoggedIn: boolean;
+  guideId: string;
+  language: string = '';
+  selectedLevel: string = '';
+  oldPassword: string = '';
+  newPassword: string = '';
 
   constructor(
     private http: HttpClient,
@@ -25,30 +28,44 @@ export class EditGuideProfileComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    if(this.tokenStorage.getUser()){
+    if (this.tokenStorage.getUser()) {
       this.currentUser = this.tokenStorage.getUser();
       this.isLoggedIn = true;
-    }
-    else {
+    } else {
       this.isLoggedIn = false;
     }
 
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.guideId = params['id']; // (+) converts string 'id' to a number
 
       // In a real app: dispatch action to load the details here.
-   });
+    });
   }
 
   saveData() {
-    // window.location.reload();
-    // this.guide.qualifications.push({
-    //   language: this.language,
-    //   level: this.selectedLevel,
-    // });
     console.log('guide profile updated with ==>', this.guide);
-    this.http.put<any>(`/api/users/guides/${this.guideId}/edit`, this.guide).subscribe((data) => {
-      console.log(data);
-    });
+    this.http
+      .put<any>(`/api/users/guides/${this.guideId}/edit`, this.guide)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+  changePassword() {
+    console.log(this.oldPassword, this.newPassword);
+    this.http
+      .put('/api/user/:id/edit', {
+        oldPassword: this.oldPassword,
+        newPassword: this.newPassword,
+      })
+
+      .subscribe(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 }
